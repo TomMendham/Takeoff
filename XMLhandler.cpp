78d1f6e1652a4360e452acd1bd1b0c755c24d8fa
Daniel <dj0407@hotmail.co.uk>
2017-04-27 00:34:00 +0100

    Added XMLhandler files

---
+++ XMLhandler.cpp
@@ -0,0 +1,94 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QStringList>
+#include <QTextStream>
+#include <QXmlStreamReader>
+#include <iostream>
+#include "flight.h"
+#include "airport.h"
+#include "XMLhandler.h"
+
+std::vector<Airport*> readAirports() {
+    QXmlStreamReader Rxml;
+
+
+    QString cont, coun, city, name, code;
+    float lon, lat;
+    std::vector<Airport*> airportVec;
+
+
+    QFile file("C:\\Users\\Daniel\\Documents\\XML_test\\Airports.xml");
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+           std::cout << "ERROR OPENING FILE" << std::endl;
+    }
+
+
+    Rxml.setDevice(&file);
+    Rxml.readNext();
+
+    while (!Rxml.atEnd()) {
+        if (Rxml.isStartElement()) {
+            if (Rxml.name() == "AIRPORTS") {
+                Rxml.readNext();
+            } else if (Rxml.name() == "AIRPORT") {
+                while (!Rxml.atEnd()) {
+                    if (Rxml.isEndElement()) {
+                        Rxml.readNext();
+                        break;
+                    } else if (Rxml.isCharacters()) {
+                        Rxml.readNext();
+                    } else if (Rxml.isStartElement()) {
+                        if (Rxml.name() == "CONTINENT") {
+
+                            cont = Rxml.readElementText();
+
+                        } else if (Rxml.name() == "COUNTRY") {
+
+                            coun = Rxml.readElementText();
+
+                        } else if (Rxml.name() == "CITY") {
+
+                            city = Rxml.readElementText();
+
+                        } else if (Rxml.name() == "NAME") {
+
+                            name = Rxml.readElementText();
+
+                        } else if (Rxml.name() == "CODE") {
+
+                            code = Rxml.readElementText();
+
+                        } else if (Rxml.name() == "LONG") {
+                            std::string temp = Rxml.readElementText().toStdString();
+                            lon = std::stof(temp);
+
+                        } else if (Rxml.name() == "LAT") {
+
+                            std::string temp = Rxml.readElementText().toStdString();
+                            lat = std::stof(temp);
+
+                        }
+
+                        Rxml.readNext();
+                    }
+                    else {
+                        Rxml.readNext();
+                    }
+                }
+            }
+
+            if (cont != "") {
+                Airport* airport = new Airport(cont, coun, city, name, code, lon, lat);
+                airportVec.push_back(airport);
+            }
+
+        }
+        else {
+            Rxml.readNext();
+        }
+    }
+
+    return airportVec;
+
+}
